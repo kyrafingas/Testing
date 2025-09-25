b1f6553272362aba71ec46925d6c4c9a533f98e3
kyrafingas <kyrafingas@gmail.com>
2025-09-25 19:29:58 -0400

    Update AddressBook.java

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -17,7 +17,7 @@ public class AddressBook {
     }
 
     public static void main(String[] args) {
-        System.out.println("Address Book Yeah");
+        System.out.println("Address Book");
         BuddyInfo buddy1 = new BuddyInfo();
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy1);
